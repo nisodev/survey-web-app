5f312ffafd666651752a1a95a1ebd0ae7750a101
salginci <ozgur.salginci@niso.com.tr>
2023-02-27 19:50:23 +0300

    survey welcome

--- components/AttendeeInfo.vue
+++ components/AttendeeInfo.vue
@@ -17,12 +17,11 @@
       <div v-if="survey.attendee" class="name mb-3">
         <h4>Survey {{ survey.survey.title }}</h4>
         <h4>Welcome {{ survey.attendee.attendetitle }}</h4>
-        <img v-if="survey.survey && survey.survey.surveyWelcomeImage && survey.survey.surveyWelcomeImage !== ''" :src="survey.survey.surveyWelcomeImage">
+        <div class="p-3">
+          <img v-if="survey.survey && survey.survey.surveyWelcomeImage && survey.survey.surveyWelcomeImage !== ''" style="border-radius: 10px" :src="survey.survey.surveyWelcomeImage">
+        </div>
         <p class="text-center" v-html="survey.statusText"></p>
       </div>
-    </div>
-    <div v-if="isOverFlown">
-      <svg  class="overflow-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--! Font Awesome Pro 6.2.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M246.6 470.6c-12.5 12.5-32.8 12.5-45.3 0l-160-160c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L224 402.7 361.4 265.4c12.5-12.5 32.8-12.5 45.3 0s12.5 32.8 0 45.3l-160 160zm160-352l-160 160c-12.5 12.5-32.8 12.5-45.3 0l-160-160c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L224 210.7 361.4 73.4c12.5-12.5 32.8-12.5 45.3 0s12.5 32.8 0 45.3z"/></svg>
     </div>
   </div>
 </template>
@@ -30,32 +29,10 @@
 <script>
 import { mapGetters } from 'vuex'
 export default {
-  data() {
-    return {
-      mounted: false,
-      scrolled: false
-    }
-  },
   computed: {
     ...mapGetters({
       survey: 'getSurvey',
     }),
-    isOverFlown() {
-      if(this.mounted && this.$refs && this.$refs.content){
-        return !this.scrolled && this.$refs.content.scrollHeight > this.$refs.content.clientHeight
-      }
-     return false
-    }
-  },
-  mounted () {
-    setTimeout(() => {
-      this.mounted = true
-    }, 300);
-  },
-  methods: {
-    onScrolled() {
-      this.$refs.content.scrollTop !== 0 ? this.scrolled = true : this.scrolled = false
-    }
   },
 }
 </script>
--- nuxt.config.js
+++ nuxt.config.js
@@ -42,6 +42,7 @@ export default {
   modules: [
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
+    '@nuxtjs/sitemap'
     // 'bootstrap-vue/nuxt',
   ],
 
